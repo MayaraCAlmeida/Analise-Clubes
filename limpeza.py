7b8d55cef146e5c3e43857f8435ffc2f790809b3
Mayara-Almeida <mayalmeida222@gmail.com>
2026-03-21 23:05:52 -0300

    revisão de código data cleaning

--- limpeza.py
+++ limpeza.py
@@ -1,22 +1,14 @@
 import pandas as pd
 
-# CAMINHO DO ARQUIVO DE ENTRADA
 df = pd.read_csv("nome_do_arquivo.csv")
 
-# Garantir tipos
 df["ano"] = df["ano"].astype(int)
 df["faturamento"] = (
-    df["faturamento"]
-    .astype(str)
+    df["faturamento"].astype(str)
     .str.replace(".", "", regex=False)
     .str.replace(",", ".", regex=False)
     .astype(float)
 )
 
-# ORDENAR DADOS POR ANO E FATURAMENTO
 df = df.sort_values(["ano", "faturamento"], ascending=[True, False])
-
-# PASTA DE SAIDA
 df.to_csv("nome_do_arquivo_limpo.csv", index=False)
-
-print("Arquivo limpo gerado!")
